7971ddddbe13faf623b63863ce44c26fe82a0225
YuhaPark0804 <danielyuha@gmail.com>
2024-11-16 21:24:49 +0900

    Update NP2024_Test1_Part2_22000287.cpp

--- tutorial/NP_test1/NP2024_Test1_Part2_22000287.cpp
+++ tutorial/NP_test1/NP2024_Test1_Part2_22000287.cpp
@@ -13,8 +13,10 @@ Description      : MidTerm2024 - Part 2 (Q4)
 #include "stdio.h"
 #include "stdlib.h"
 #include <math.h>
-#include "../../include/myNP_22000287.h"
+#include "../../../include/myNP_22000287.h"
 
+
+#define N 12
 
 double func_Q4a(double x);
 double func_Q4c(double x);
@@ -25,55 +27,58 @@ double integralQuadComp(double func(double _x), double a, double b, int n);
 
 int main(int argc, char* argv[])
 {
-	
+
 	printf("\n**************************************************");
 	printf("\n|                 Question 4-(a).                |");
 	printf("\n**************************************************\n");
 
 	double I1 = 0.0;
 
-	// [TO-DO] Add your code here
-	// [TO-DO] Add your code here
-	// [TO-DO] Add your code here
+	double w0 = 5.0 / 9.0;
+	double w1 = 8.0/ 9.0;
+	double w2 = 5.0 / 9.0;
+
+	double t0 = -1.0 * sqrt(1.0 / 3.0);
+	double t1 = 0.0;
+	double t2 = sqrt(1.0 / 3.0);
+	
+
+	I1 = integralQuad(func_Q4a);
+
 
 	printf("\nanswer of Q4-(a) = %f\n\n", I1);
 
 
 
-	
+
 	printf("\n**************************************************");
 	printf("\n|                 Question 4-(b).                |");
 	printf("\n**************************************************\n");
-	
-	double a    = 0.0;
-	double b    = 3.0;
+
+	double a = 0.0;
+	double b = 3.0;
 	double I2 = 0.0;
 
-	// [TO-DO] Add your code here
-	// [TO-DO] Add your code here
-	// [TO-DO] Add your code here
+	I2 = integralQuad2(func_Q4a, a, b);
+
 
 	printf("\nanswer of Q4-(b) = %f\n\n", I2);
-	
-	
 
 
-	
+
 	printf("\n**************************************************");
 	printf("\n|                 Question 4-(c).                |");
 	printf("\n**************************************************\n");
-	
-	double a    = 0.0;
-	double b    = 1.2;
-	int    n    = 12;
+
+	double A = 0.0;
+	double B = 1.2;
+	int    n = 12;
 	double z_elong = 0.0;
 
-	
-	// [TO-DO] Add your code here
-	// [TO-DO] Add your code here
-	// [TO-DO] Add your code here
-	
-		
+
+	z_elong = integralQuadComp(func_Q4c, A, B, n);
+
+
 	printf("\nanswer of Q4_c = %f\n\n", z_elong);
 
 
@@ -90,15 +95,10 @@ int main(int argc, char* argv[])
 
 double func_Q4a(double x)
 {
-	
-	int N_MAX = 10;
-	double out = 0.0;
-	double x = _x;
 
-	for (int k = 0; k < N_MAX; k++) {
-		out += pow(x, k) / factorial(k);
-	}
-	return S_N;
+	double out = 0.0;
+	
+	out = exp(-1*pow(x, 2));
 
 	return out;
 }
@@ -107,45 +107,66 @@ double func_Q4c(double x)
 {
 	double out = 0.0;
 
-	// [TO-DO] Add your code here
-	// [TO-DO] Add your code here
+	double ep = 40 * pow(10, -3) * pow(x, 0.5);
+	double L = 1.2; //[m]
+
+	out = (1 + ep);
 
 	return out;
 }
 
 double integralQuad(double func(double _x))
 {
-	double weight[3] = {5.0 / 9.0, 8.0 / 9.0, 5.0 / 9.0};
-	double xn[3]     = {-sqrt(1.0 / 3.0), 0.0, sqrt(1.0 / 3.0)};
+	double weight[3] = { 5.0 / 9.0, 8.0 / 9.0, 5.0 / 9.0 };
+	double xn[3] = { -sqrt(1.0 / 3.0), 0.0, sqrt(1.0 / 3.0) };
 
 	double output = 0.0;
+	double output1 = 0.0;
+	double output2 = 0.0;
+	double output3 = 0.0;
 
-	// [TO-DO] Add your code here
-	// [TO-DO] Add your code here
+	for (int i = 0; i < 3; i++) {
+		output += weight[i] * func(xn[i]);
+	}
 
 	return output;
 }
 
 double integralQuad2(double func(double _x), double a, double b)
 {
-	//double weight[3] = { 5.0 / 9.0, 8.0 / 9.0, 5.0 / 9.0 };
-	//double xn[3] = { -sqrt(1.0 / 3.0), 0.0, sqrt(1.0 / 3.0) };
+	double weight[3] = { 5.0 / 9.0, 8.0 / 9.0, 5.0 / 9.0 };
+	double xn[3] = { -sqrt(1.0 / 3.0), 0.0, sqrt(1.0 / 3.0) };
 
 	double output = 0.0;
 
-	// [TO-DO] Add your code here
-	// [TO-DO] Add your code here
 
+	for (int i = a; i < b; i++) {
+		output += weight[i] * func(0.5 * ((b - a) * xn[i] + a + b)) * 0.5 * (b - a);
+	}
+	
 	return output;
 }
 
 double integralQuadComp(double func(double _x), double a, double b, int n)
 {
-	double output = 0.0;
 
-	// [TO-DO] Add your code here
-	// [TO-DO] Add your code here
+	double weight[3] = { 5.0 / 9.0, 8.0 / 9.0, 5.0 / 9.0 };
+	double xn[3] = { -sqrt(1.0 / 3.0), 0.0, sqrt(1.0 / 3.0) };
 
-	return output;
+	double output[N] = { 0.0 };
+	double out = 0.0;
+
+	for (int k = 0; k < n; k++) {
+		for (int i = a; i < b; i++) {
+			output[k] += weight[i] * func(0.5 * ((b - a) * xn[i] + a + b)) * 0.5 * (b - a);
+		}
+	}
+
+	for (int i = 0; i < n; i++) {
+		out += output[i];
+	}
+
+
+	return out;
 
 }
